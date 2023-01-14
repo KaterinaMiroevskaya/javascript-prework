791645c47d91a6f89b313329c04052f3399b98eb
Kateryna Miroievska <miroevskaya.k@gmail.com>
2023-01-14 20:25:40 +0100

    add computerMove

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-let computerMove = "kamień";
-let playerMove = "papier";
-printMessage(
-  "Zagrałem " +
-    computerMove +
-    "! Jeśli Twój ruch to " +
-    playerMove +
-    ", to wygrywasz!"
-);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = "nieznany ruch";
+if (randomNumber == 1) {
+  computerMove = "kamień";
+} else if (randomNumber == 2) {
+  computerMove = "papier";
+} else {
+  computerMove = "nożyce";
+}
