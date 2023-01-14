108416850c04377c7a7f2a600300874494fba8a0
Kateryna Miroievska <miroevskaya.k@gmail.com>
2023-01-14 20:29:09 +0100

    add playerMove

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ if (randomNumber == 1) {
 } else {
   computerMove = "nożyce";
 }
+let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+
+let playerMove = "nieznany ruch";
+
+if (playerInput == "1") {
+  playerMove = "kamień";
+} else if (playerInput == "2") {
+  playerMove = "papier";
+} else if (playerInput == "3") {
+  playerMove = "nożyce";
+}
