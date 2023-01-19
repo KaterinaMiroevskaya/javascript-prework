3a41cf9c53af8cb244450ff112b57cf518e0541b
Kateryna Miroievska <miroevskaya.k@gmail.com>
2023-01-19 09:48:55 +0100

    add functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,61 @@
+function getMoveName(argMoveId) {
+  if (argMoveId == 1) {
+    return "камень";
+  }
+  if (argMoveId == 2) {
+    return "бумага";
+  }
+  if (argMoveId == 3) {
+    return "ножницы";
+  }
+
+  printMessage("Не известен ход с id " + argMoveId + ".");
+  return "неизвестный ход";
+}
+function displayResult(argComputerMove, argPlayerMove) {
+  if (argComputerMove == "камень" && argPlayerMove == "бумага") {
+    return "Ты выиграл!";
+  }
+  if (argComputerMove == "бумага" && argPlayerMove == "ножницы") {
+    return "Ты выиграл!";
+  }
+  if (argComputerMove == "ножницы" && argPlayerMove == "камень") {
+    return "Ты выиграл!";
+  }
+  if (argComputerMove == "камень" && argPlayerMove == "камень") {
+    return "Ничья!";
+  }
+  if (argComputerMove == "бумага" && argPlayerMove == "бумага") {
+    return "Ничья!";
+  }
+  if (argComputerMove == "ножницы" && argPlayerMove == "ножницы") {
+    return "Ничья!";
+  }
+  return "Ты проиграл!";
+}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = "неизвестный ход";
-if (randomNumber == 1) {
+let computerMove = getMoveName(randomNumber);
+/*if (randomNumber == 1) {
   computerMove = "камень";
 } else if (randomNumber == 2) {
   computerMove = "бумага";
 } else {
   computerMove = "ножницы";
-}
+}*/
 let playerInput = prompt("Выбери свой ход! 1: камень, 2: бумага, 3: ножницы.");
 
-let playerMove = "неизвестный ход";
+let playerMove = getMoveName(playerInput);
+let result = displayResult(computerMove, playerMove);
+printMessage(result);
 
-if (playerInput == "1") {
+/*if (playerInput == "1") {
   playerMove = "камень";
 } else if (playerInput == "2") {
   playerMove = "бумага";
 } else if (playerInput == "3") {
   playerMove = "ножницы";
-}
-if (computerMove == "камень" && playerMove == "бумага") {
+}*/
+/*if (computerMove == "камень" && playerMove == "бумага") {
   printMessage("Ты выиграл!");
 } else if (computerMove == "бумага" && playerMove == "ножницы") {
   printMessage("Ты выиграл!");
@@ -32,4 +69,5 @@ if (computerMove == "камень" && playerMove == "бумага") {
   printMessage("Ничья!");
 } else if (playerInput <= "0" || playerInput > "3") {
   printMessage("Нужно выбрать ход от 1 до 3!");
-} else printMessage("Ты проиграл!");
+} else printMessage("Ты проиграл!");*/
+console.log(playerMove, computerMove);
