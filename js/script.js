9ace5afe8e75d52077705ccb1034320544ed0e17
Kateryna Miroievska <miroevskaya.k@gmail.com>
2023-01-23 20:51:08 +0100

    deleted comments

--- js/script.js
+++ js/script.js
@@ -37,46 +37,11 @@ function playGame(playerInput) {
   }
   let randomNumber = Math.floor(Math.random() * 3 + 1);
   let computerMove = getMoveName(randomNumber);
-  /*if (randomNumber == 1) {
-  computerMove = "камень";
-} else if (randomNumber == 2) {
-  computerMove = "бумага";
-} else {
-  computerMove = "ножницы";
-}*/
-  /* let playerInput = prompt(
-    "Выбери свой ход! 1: камень, 2: бумага, 3: ножницы."
-  );*/
-
   let playerMove = getMoveName(playerInput);
   let result = displayResult(computerMove, playerMove);
   printMessage(result);
-
-  /*if (playerInput == "1") {
-  playerMove = "камень";
-} else if (playerInput == "2") {
-  playerMove = "бумага";
-} else if (playerInput == "3") {
-  playerMove = "ножницы";
-}*/
-  /*if (computerMove == "камень" && playerMove == "бумага") {
-  printMessage("Ты выиграл!");
-} else if (computerMove == "бумага" && playerMove == "ножницы") {
-  printMessage("Ты выиграл!");
-} else if (computerMove == "ножницы" && playerMove == "камень") {
-  printMessage("Ты выиграл!");
-} else if (computerMove == "камень" && playerMove == "камень") {
-  printMessage("Ничья!");
-} else if (computerMove == "бумага" && playerMove == "бумага") {
-  printMessage("Ничья!");
-} else if (computerMove == "ножницы" && playerMove == "ножницы") {
-  printMessage("Ничья!");
-} else if (playerInput <= "0" || playerInput > "3") {
-  printMessage("Нужно выбрать ход от 1 до 3!");
-} else printMessage("Ты проиграл!");*/
   console.log(playerMove, computerMove);
 }
-// document.getElementById("play-rock").addEventListener("click", playGame(1));
 let playRock = document.getElementById("play-rock");
 playRock.addEventListener("click", function () {
   playGame(1);
